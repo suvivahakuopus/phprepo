d6e6ec9305c9b6f157fa6cc95848b6a57a90fc12
suvivahakuopus <81032707+suvivahakuopus@users.noreply.github.com>
2021-11-07 22:44:56 +0200

    Testi

---
+++ db.sql
@@ -0,0 +1,14 @@
+drop database if exists shoppinglist;
+
+create database shoppinglist;
+
+use shoppinglist;
+
+create table item (
+    id int primary key auto_increment, 
+    description varchar(255) not null,
+    amount smallint unsigned not null
+);
+
+insert into into item (description, amount) values ('Test item', 1); 
+
